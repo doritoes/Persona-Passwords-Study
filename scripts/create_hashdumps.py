6f82870a9fe1c0c4e356a3451d60ae19e664e1e4
doritoes <39832079+doritoes@users.noreply.github.com>
2026-01-04 22:44:15 -0500

    Update create_hashdumps.py

--- scripts/create_hashdumps.py
+++ scripts/create_hashdumps.py
@@ -4,7 +4,8 @@ Create example shadow files and pwdump files for testing
 
 pip install passlib
 """
-from passlib.hash import sha512_crypt, nthash
+import hashlib from passlib.hash
+import sha512_crypt, nthash
 
 # Input Data
 data = [
@@ -38,11 +39,32 @@ shadow_output = []
 pwdump_output = []
 start_uid = 1001
 
+md5_list = []
+sha1_list = []
+sha256_list = []
+
 for i, (user, password) in enumerate(data):
     shadow_output.append(generate_shadow_line(user, password))
     pwdump_output.append(generate_pwdump_line(user, password, start_uid + i))
+    md5_list.append(hashlib.md5(password.encode()).hexdigest())
+    sha1_list.append(hashlib.sha1(password.encode()).hexdigest())
+    sha256_list.append(hashlib.sha256(password.encode()).hexdigest())
 
 print("--- SHADOW FILE CONTENT ---")
 print("\n".join(shadow_output))
 print("\n--- PWDUMP FILE CONTENT ---")
 print("\n".join(pwdump_output))
+print("--- MD5 CONTENT ---")
+print("\n".join(sha1_list))
+print("\n".join(pwdump_output))
+print("--- SHA1 CONTENT ---")
+print("\n".join(sha1_list))
+print("--- SHA256 CONTENT ---")
+print("\n".join(sha256_list))
+
+with open("md5.txt", "w") as f:
+    f.write("\n".join(md5_list))
+with open("sha1.txt", "w") as f:
+    f.write("\n".join(sha1_list))
+with open("sha256.txt", "w") as f:
+    f.write("\n".join(sha256_list))
